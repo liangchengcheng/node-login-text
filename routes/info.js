6e99ecc46673f8c57c31052024a4e88c22e85fb6
liangchengcheng <1038127753@qq.com>
2016-01-14 23:05:10 +0800

    sql简单操作
    sql简单操作

--- routes/info.js
+++ routes/info.js
@@ -1,8 +1,51 @@
 /**
  * Created by lcc on 16/1/13.
  */
-//等待添加
-//var express=require('express');
+
+var mysql=require('mysql');
+
+/**
+ * 从数据库获取数据
+ */
+function getDatFromDB(datacallback){
+    var connection=mysql.createConnection({
+        host :'127.0.0.1',
+        user :'root',
+        password :'123',
+        port : '3306',
+        database :'mydb'
+    });
+    //开始连接
+    connection.connect();
+    var querySql="select * from userinfo";
+    connection.query(querySql,function(err,result,fields){
+        if(err){
+            connection.close();
+            return;
+        }
+
+        for(var i=0;i<result.length;i++){
+            var rlt=result[i];
+            var age=rlt['age'];
+        }
+        datacallback(rlt);
+    });
+}
+
 module.exports=function(request,response,next){
-  response.render('info');
+    getDatFromDB();
+    console.log("进入 info 路由");
+
+    getDatFromDB(function(result){
+
+
+        for(var i=0;i<result.length;i++){
+            var rlt=result[i];
+            var age=rlt['age'];
+        }
+
+        response.render('info',{
+            'queryRlt': result
+        });
+    })
 };
